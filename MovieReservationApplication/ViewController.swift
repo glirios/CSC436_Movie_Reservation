8030831957e9bf8b8e410af056005fba44d467cf
glirios <lirios311@gmail.com>
2017-11-09 14:23:23 -0800

    Initial Commit

---
+++ MovieReservationApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieReservationApplication
+//
+//  Created by Giovanni Lirios Aguilar on 11/9/17.
+//  Copyright © 2017 Giovanni Lirios Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
